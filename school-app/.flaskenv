aa35c0462aeefee792a354f6b18d9b7eb4a63f9f
stupns <stupns@i.ua>
2023-03-14 14:59:35 +0200

    add all files to branch BE-7

--- school-app/.flaskenv
+++ school-app/.flaskenv
@@ -1,1 +1,7 @@
 FLASK_APP=main.py
+
+## Database
+MYSQL_HOST=localhost
+MYSQL_USER=stupns
+MYSQL_PASSWORD=hzOMT8yii
+MYSQL_DB=school_api
--- school-app/config.py
+++ school-app/config.py
@@ -1,14 +1,19 @@
 import os
-
+from pydantic import BaseSettings
 from dotenv import load_dotenv
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(BASEDIR, '.env'))
 
 
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '\q\xbfq\x97\xbf\t}\xec\x8c/\xcd\xb5V\x0e\x93\x81\xa0\xa9\xd0I=s\x17'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(BASEDIR, 'school-app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    LANGUAGES = ['en', 'uk']
+class DBConfig(BaseSettings):
+    MYSQL_HOST: str
+    MYSQL_USER: str
+    MYSQL_PASSWORD: str
+    MYSQL_DB: str
+
+    class Config:
+        env_file = '.env'
+
+
+database_settings = DBConfig()
